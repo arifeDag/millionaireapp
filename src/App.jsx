d4e8d3f6a353e93275c13fa4341f10df9138405d
Arife Kara <arifedagkara@gmail.com>
2022-12-18 23:33:50 +0300

    resim yüklendi divler olusturuldu

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+function App() {
+  return (
+    <div className="app">
+       <div className="main">main</div>
+       <div className="pyramid">money</div>
+       
+    
+        
+    </div>
+  );
+}
+
+export default App;
